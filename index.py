b875b7af26a96c1ec7101e3f6a37ef4c4382ca97
NotPunz1 <92071015+NotPunz1@users.noreply.github.com>
2021-10-30 15:00:41 +0700

    add index.py

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-print("hello")
+#pip install pygame
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello world!')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
